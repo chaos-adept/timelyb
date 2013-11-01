8a1b21d50bf42fa2379bd4a26b22d54dc81230c2
Denis Rykovanov <denis.rykovanov@gmail.com>
2013-11-01 09:45:57 +0300

    [+] select activity from list

--- back-end/helloword-server/timelyb-helloword-server/main.py
+++ back-end/helloword-server/timelyb-helloword-server/main.py
@@ -20,15 +20,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        user = users.get_current_user()
-
-        if user:
-            template_values = { 'nickName': user.nickname() }
-
-            template = JINJA_ENVIRONMENT.get_template('/templates/logEvent.html')
-            self.response.write(template.render(template_values))
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
+        self.redirect('/pages/logEvent.html')
 
 
 class LogEventPage(webapp2.RequestHandler):
